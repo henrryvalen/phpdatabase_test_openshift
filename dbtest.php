15a224d23956e0972cd65874252972796c1a1be2
henrryvalen <henrryvalen@gmail.com>
2018-02-11 23:45:34 -0500

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,7 +5,11 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-echo "$dbhost";
+echo "MYSQL_SERVICE_HOST"."$dbhost";
+echo "MYSQL_SERVICE_PORT"."$dbhost";
+echo "databaseuser"."$dbuser";
+echo "databasepassword"."$dbpwd";
+echo "databasename"."$dbname";
 /*
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
